79e66c618151767b8ab69b5aed6fb6b236632f52
user.name:"anuj <anujkhare255@gmail.com>
2026-02-18 00:09:43 +0530

    bug2

--- frontendx/main.jsx
+++ frontendx/main.jsx
@@ -6,14 +6,22 @@ import { Provider } from "react-redux";
 import store from "./src/store/store";
 import { BrowserRouter } from "react-router-dom";
 
+if (import.meta.env.PROD) {
+  console.log = () => { };
+  console.debug = () => { };
+  console.info = () => { };
+  console.warn = () => { };
+  // Keeping console.error to help debugging in production if needed
+}
+
 createRoot(document.getElementById('root')).render(
 
-   <BrowserRouter>
-     <StrictMode>
-   <Provider store={store}>
-   <App />
-</Provider>
-  </StrictMode>
-   </BrowserRouter>
- 
+  <BrowserRouter>
+    <StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </StrictMode>
+  </BrowserRouter>
+
 )
