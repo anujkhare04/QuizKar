00560e8a0476c635e32ac6e7ddf0633ca9d2341d
user.name:"anuj <anujkhare255@gmail.com>
2026-06-19 02:09:18 +0530

    depolyfix

--- frontendx/src/axios/axiosinstance.jsx
+++ frontendx/src/axios/axiosinstance.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const isProd = import.meta.env.PROD;
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
 export default axiosInstance;
+ 
