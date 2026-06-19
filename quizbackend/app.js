2850c61671b012195b65ecf09c376ce06502b44a
user.name:"anuj <anujkhare255@gmail.com>
2026-06-19 19:24:04 +0530

    fix6

--- quizbackend/app.js
+++ quizbackend/app.js
@@ -16,7 +16,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "https://quiz-website-swart-seven.vercel.app",
+      "https://quiz-website-gules-seven.vercel.app",
        ],
     credentials: true,
   })
